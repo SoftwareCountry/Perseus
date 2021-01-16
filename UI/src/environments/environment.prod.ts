4e791e7d9590d15de82ef4c540f89e6da4d7be15
ARCADIA\maria.dolotova <maria.dolotova@arcadia.spb.ru>
2021-01-16 15:00:30 +0300

    changes in urls

--- UI/src/environments/environment.prod.ts
+++ UI/src/environments/environment.prod.ts
@@ -12,9 +12,9 @@ const CONCEPT_TABLES = [
 
 export const environment = {
   production: true,
-  url: 'http://10.110.1.76/api',
+  url: 'http://10.110.1.2/api',
   conceptTables: CONCEPT_TABLES,
   config: '',
-  whiteRabbitUrl: 'http://10.110.1.7',
-  cdmBuilderUrl: 'http://10.110.1.7:9000'
+  whiteRabbitUrl: 'http://10.110.1.2',
+  cdmBuilderUrl: 'http://10.110.1.2:9000'
 };
