d736fff4122ca57f9731379d0c2f120e47990e08
Arcuser <arcuser@jnjcdmu1.arcadia.intranet>
2021-04-01 16:14:25 +0300

    fix

--- CICD/ssh_deploy.sh
+++ CICD/ssh_deploy.sh
@@ -6,4 +6,4 @@ component=$3
 env=$4
 
 echo Deploy [$component] to [$host] with user [$sshUser], env=[$env]. 
-tar cf - perseus.sh perseus.h repo_pwd | ssh $sshUse@$host "tar xf -; bash perseus.sh deploy $component $env"
+tar cf - perseus.sh perseus.h repo_pwd | ssh "$sshUser"@"$host" "tar xf -; bash perseus.sh deploy $component $env"
